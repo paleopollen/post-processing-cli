b9e5b2d2fa6bf68569d1bd97e5db05fc32c79322
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2024-10-09 18:23:07 +0530

    Update config downloads URL.

--- sam2_configs/download.sh
+++ sam2_configs/download.sh
@@ -1,7 +1,7 @@
 
 # Download SAM-2 configs
 
-wget -nc -O sam2_hiera_b+.yaml https://raw.githubusercontent.com/facebookresearch/sam2/refs/heads/main/sam2_configs/sam2_hiera_b%2B.yaml
-wget -nc -O sam2_hiera_l.yaml https://raw.githubusercontent.com/facebookresearch/sam2/refs/heads/main/sam2_configs/sam2_hiera_l.yaml
-wget -nc -O sam2_hiera_s.yaml https://raw.githubusercontent.com/facebookresearch/sam2/refs/heads/main/sam2_configs/sam2_hiera_s.yaml
-wget -nc -O sam2_hiera_t.yaml https://raw.githubusercontent.com/facebookresearch/sam2/refs/heads/main/sam2_configs/sam2_hiera_t.yaml
+wget -nc -O sam2_hiera_b+.yaml https://github.com/facebookresearch/sam2/blob/0f6515ae853c40420ea8e3dd250f8031bbf03023/sam2_configs/sam2_hiera_b%2B.yaml
+wget -nc -O sam2_hiera_l.yaml https://github.com/facebookresearch/sam2/blob/0f6515ae853c40420ea8e3dd250f8031bbf03023/sam2_configs/sam2_hiera_l.yaml
+wget -nc -O sam2_hiera_s.yaml https://github.com/facebookresearch/sam2/blob/0f6515ae853c40420ea8e3dd250f8031bbf03023/sam2_configs/sam2_hiera_s.yaml
+wget -nc -O sam2_hiera_t.yaml https://github.com/facebookresearch/sam2/blob/0f6515ae853c40420ea8e3dd250f8031bbf03023/sam2_configs/sam2_hiera_t.yaml
