1b6a3f5f872b16331e9cbdf9bf8c7a65292135db
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2025-04-25 23:23:46 -0500

    Add copyright notice.

---
+++ sam2_configs/__init__.py
@@ -0,0 +1,14 @@
+#  Copyright 2025 The Board of Trustees of the University of Illinois. All Rights Reserved.
+#
+#  Licensed under the terms of Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  The License is included in the distribution as LICENSE file.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
--- src/post_processing_cli.py
+++ src/post_processing_cli.py
@@ -1,3 +1,18 @@
+#  Copyright 2025 The Board of Trustees of the University of Illinois. All Rights Reserved.
+#
+#  Licensed under the terms of Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  The License is included in the distribution as LICENSE file.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import argparse
 import logging
 
@@ -7,14 +22,15 @@ from post_processor import PostProcessor
 
 
 class PostProcessingCli:
-    def __init__(self,):
+    def __init__(self, ):
         self.parser = argparse.ArgumentParser(description="Post process the pollen detection results.")
         self.args = None
 
         # Add arguments
 
         # Detections directory path
-        self.parser.add_argument("--detections-dir", "-d", type=str, dest="detections_dir_path", nargs='?', default=None,
+        self.parser.add_argument("--detections-dir", "-d", type=str, dest="detections_dir_path", nargs='?',
+                                 default=None,
                                  required=True, help="Full path of the directory containing the detection results.")
         # SAM-2 model path
         self.parser.add_argument("--sam2-model-path", "-m", type=str, dest="sam2_model_path", nargs='?', default=None,
